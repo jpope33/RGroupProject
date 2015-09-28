661f0a456423c0094d00619119a4ed9934f95f56
Jonathan <popejb1116@gmail.com>
2015-09-27 20:07:50 -0400

    Added CSV function to IOscript.R

--- RProjectFolder/IOscript.R
+++ RProjectFolder/IOscript.R
@@ -1,3 +1,6 @@
-#This script imports and displays a tab delimited file
-importedData=read.table(file.choose(), header = T, sep = "\t")
-importedData
+#This script imports and displays a tab delimited file and a comma seperated file
+importedDataTAB=read.table(file.choose(), header = T, sep = "\t")
+importedDataTAB
+
+importedDataCSV=read.csv(file.choose(), header = T)
+importedDataCSV
