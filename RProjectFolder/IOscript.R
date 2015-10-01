65033a57df58963e78adbf1e2bd1e5be0d719550
Jonathan <popejb1116@gmail.com>
2015-09-30 22:44:39 -0400

    Added export formatting to IOscript, final edit

--- RProjectFolder/IOscript.R
+++ RProjectFolder/IOscript.R
@@ -6,12 +6,17 @@ importedDataTAB
 importedDataCSV=read.csv(file.choose(), header = T)
 importedDataCSV
 
-Names=c("Mike", "Susan", "David", "Thomas", "Nick", "Dana", "Simon", "Lisa")
+Names=c("Mike", "Susan", "David", "Tom", "Nick", "Dana", "Simon", "Lisa")
 Gender=c("M","F","M","M", "M", "F","M","F")
 Age=c(27,33,44,39,33,26,44,49)
 Smoker=c(F,F,T,F,T,F,F,T)
 StudyParticipants=data.frame(Names, Gender, Age, Smoker)
 
-#Exports file as tab delimited file
-write.table(StudyParticipants, "/Users/jonathanpope/Desktop/writeTo", sep="\t")
+#Exports file as tab delimited file named testTAB
+write.table(StudyParticipants, "/Users/jonathanpope/Desktop/writeTo/testTAB.txt", sep="\t", row.names = F)
 
+#More robust export that retains column alignment format
+capture.output(print(StudyParticipants, print.gap=3), file="/Users/jonathanpope/Desktop/writeTo/testTABformat.txt")
+
+#Exports table as comma separated file named testCSV
+write.csv(StudyParticipants, "/Users/jonathanpope/Desktop/writeTo/testCSV.csv")
